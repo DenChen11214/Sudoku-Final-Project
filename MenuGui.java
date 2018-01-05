34cd738c9b9800adb5a2fdc5916684a75fa45554
Dennis Chen <dchen22@stuy.edu>
2018-01-04 21:20:46 -0500

    added fields for the menu

---
+++ MenuGui.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class MenuGui extends JFrame implements ActionListener{
+  private JButton start;
+  private JComboBox difficultyMenu;
+  private String[] difficulty = {"Easy","Medium","Hard"};
+  private JLabel sudoku;
+  private JLabel seed;
+  private JLabel difficultyLabel;
+  private JTextField seedNumber;
+  private Container pane;
+  public void actionPerformed(ActionEvent e){
+
+  }
+}
