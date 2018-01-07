c5490182f39a46759e40313aee946ebcb589b8e6
Anthony <alam6@stuy.edu>
2018-01-07 18:54:04 -0500

    added sizes for textfields

--- puzzleGUI.java
+++ puzzleGUI.java
@@ -12,6 +12,7 @@ public class puzzleGUI extends JFrame implements ActionListener{
     private JButton backToMenu;
     private JButton reset;
     private Container pane;
+    private JLabel result;
     public puzzleGUI(){
 	pane = this.getContentPane();
 	pane.setLayout(new FlowLayout());
@@ -20,28 +21,36 @@ public class puzzleGUI extends JFrame implements ActionListener{
 	this.setLocation(0,0);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	seed = new JLabel("Seed:");
-	seedNumber = new JTextField("seedNumber");
-	checkText = new JTextField("checkText");
+	result = new JLabel("Result:");
+	seedNumber = new JTextField(10);
+	checkText = new JTextField(6);
 	hint = new JButton("hint");
 	check = new JButton("check");
 	solution = new JButton("solution");
 	backToMenu = new JButton("backToMenu");
 	reset = new JButton("reset");
-	pane.add(seed);
-	pane.add(seedNumber);
 	for (int i=0;i<9;i++){
 		for (int ii=0; ii<9; ii++){
+			puzzle[i][ii]=new JTextField(8);
 			pane.add(puzzle[i][ii]);
 		}
 	}
+	pane.add(result);
 	pane.add(checkText);
 	pane.add(hint);
 	pane.add(check);
 	pane.add(solution);
 	pane.add(backToMenu);
 	pane.add(reset);
+	pane.add(seed);
+	pane.add(seedNumber);
     }
     public void actionPerformed(ActionEvent e){
+    }
+    public static void main(String[] args) {
+    puzzleGUI m = new puzzleGUI();
+    m.setVisible(true);
+
     }
 }
 	
