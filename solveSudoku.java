4b7b391b27c46bed8ea1bfeeb365663d5c29c6a8
Dennis Chen <dchen22@stuy.edu>
2018-01-08 17:27:42 -0500

    created part of boxSafe that gives the boxrow and boxcol

--- solveSudoku.java
+++ solveSudoku.java
@@ -19,4 +19,34 @@ public class solveSudoku{
 		}
 		return true;
 	}
+  //checks whether or not a certain number is already in the box
+  //the box number goes horizontally to the next row
+  public static int boxSafe(int box, int num){
+    int boxRow = 0;
+    int boxCol = 0;
+    for(int i = box; i >0 ;i -= 3){
+      boxRow++;
+      if(i <= 0){
+        i = 0;
+      }
+    }
+    if(box <= 3){
+      boxCol = box;
+    }
+    for(int i = box; i > 0;i-=3){
+      boxCol = i;
+    }
+    return boxCol;
+  }
+  public static void main(String[] args){
+    System.out.println(boxSafe(1,0));
+    System.out.println(boxSafe(2,0));
+    System.out.println(boxSafe(3,0));
+    System.out.println(boxSafe(4,0));
+    System.out.println(boxSafe(5,0));
+    System.out.println(boxSafe(6,0));
+    System.out.println(boxSafe(7,0));
+    System.out.println(boxSafe(8,0));
+    System.out.println(boxSafe(9,0));
+  }
 }
