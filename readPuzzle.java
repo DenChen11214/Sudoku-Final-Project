b585fd948d065eaa13ca1a23c0dfcc1b8e1ba4f7
Anthony <alam6@stuy.edu>
2018-01-09 14:41:17 -0500

    started readPuzzle class

---
+++ readPuzzle.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Random;
+public class readPuzzle{
+    int[][] solution = new int[9][9];
+    public readPuzzle{
+    }
+}
+	
