d4210b7c6b2ca06c08ae4bf1704bda9ce44a4e94
Anthony <alam6@stuy.edu>
2018-01-09 20:49:29 -0500

    added main method to test

--- readPuzzle.java
+++ readPuzzle.java
@@ -10,13 +10,29 @@ public class readPuzzle{
 	    	Scanner in = new Scanner(f).useDelimiter(",");//into one line
 	    	for (int i=0;i<9;i++){
 	    		for (int ii=0;ii<9;ii++){
-	    			solution[i][ii]= in.next();
+	    			if (in.hasNext()){
+	    				solution[i][ii]= in.next();
+	    			}
 	    		}
 	    	}
 		}catch(FileNotFoundException e){
 	    	System.out.println("File not found");
 	    	System.exit(1);
 		}
+    }
+    public String toString(){
+	String ans="";
+	for (int row = 0; row < 9; row++){
+	    for (int col = 0; col < 9; col++){
+		ans+="" + solution[row][col];
+		ans+=("\n");
+	    }
+	}
+	return ans;
+    }
+    public static void main(String[] args){
+    	readPuzzle a = new readPuzzle();
+    	System.out.println(a);
     }
 }
 	
