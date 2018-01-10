3376d2dd192ac77397e97e7b8a385bcb50428a6d
Anthony <alam6@stuy.edu>
2018-01-10 10:30:19 -0500

    added getPuzzle and getSolution methods

--- readPuzzle.java
+++ readPuzzle.java
@@ -3,22 +3,37 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Random;
 public class readPuzzle{
-    private String[][] solution = new String[9][9];
+private String[][] puzzle =new String[9][9];
+private String[][] solution = new String[9][9];
     public readPuzzle(){
     	try{
-	    	File f = new File("1.csv");//can combine
-	    	Scanner in = new Scanner(f).useDelimiter(",");//into one line
+	    File f = new File("1.csv");//can combine
+	    Scanner in = new Scanner(f).useDelimiter(",");//into one line
 	    	for (int i=0;i<9;i++){
-	    		for (int ii=0;ii<9;ii++){
-	    			if (in.hasNext()){
-	    				solution[i][ii]= in.next();
-	    			}
-	    		}
+		    for (int ii=0;ii<9;ii++){
+			if (in.hasNext()){
+			    puzzle[i][ii]= in.next();
+			}
+		    }
 	    	}
-		}catch(FileNotFoundException e){
-	    	System.out.println("File not found");
-	    	System.exit(1);
-		}
+	}catch(FileNotFoundException e){
+	    System.out.println("File not found");
+	    System.exit(1);
+	}
+	try{
+	    File f = new File("1 solution.csv");//can combine
+	    Scanner in = new Scanner(f).useDelimiter(",");//into one line
+	    	for (int i=0;i<9;i++){
+		    for (int ii=0;ii<9;ii++){
+			if (in.hasNext()){
+			    solution[i][ii]= in.next();
+			}
+		    }
+	    	}
+	}catch(FileNotFoundException e){
+	    System.out.println("File not found");
+	    System.exit(1);
+	}
     }
     public String toString(){
 	String ans="";
@@ -30,7 +45,25 @@ public class readPuzzle{
 	}
 	return ans;
     }
-    public static void main(String[] args){
+    public String[][] getPuzzle(){
+		String[][] ans= new String[9][9];
+		for (int i=0;i<9;i++){
+		    for (int ii=0;ii<9;ii++){
+		    	ans[i][ii]=puzzle[i][ii];
+		    }
+		}
+		return ans;
+	}
+	public String[][] getSolution(){
+		String[][] ans= new String[9][9];
+		for (int i=0;i<9;i++){
+		    for (int ii=0;ii<9;ii++){
+		    	ans[i][ii]=solution[i][ii];
+		    }
+		}
+		return ans;
+	}
+	public static void main(String[] args){
     	readPuzzle a = new readPuzzle();
     	System.out.println(a);
     }
