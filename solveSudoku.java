4a294c2769a918e7208ecd5026fb3c6c54b05b25
Anthony <alam6@stuy.edu>
2018-01-19 14:26:47 -0500

    finished the isSafe method

--- solveSudoku.java
+++ solveSudoku.java
@@ -1,22 +1,38 @@
 public class solveSudoku{
-	private int[][] grid;
+    private int[][] grid;
 
-	//checks whether or not a certain number (num) is already in the row
-	public boolean rowsafe(int row, int num){
-		for (int col =0; col < 9; col++){
-			if (grid[row][col] == num){
-				return false;
-			}
-		}
-		return true;
+    //checks whether or not a certain number (num) is already in the row
+    public boolean rowsafe(int row, int num){
+	for (int col =0; col < 9; col++){
+	    if (grid[row][col] == num){
+		return false;
+	    }
 	}
-	//checks whether or not a certain number (num) is already in the column
-	public boolean colsafe(int col, int num){
-		for (int row =0; row < 9; row++){
-			if (grid[row][col] == num){
-				return false;
-			}
-		}
-		return true;
+	return true;
+    }
+    //checks whether or not a certain number (num) is already in the column
+    public boolean colsafe(int col, int num){
+	for (int row =0; row < 9; row++){
+	    if (grid[row][col] == num){
+		return false;
+	    }
 	}
+	return true;
+    }
+    public boolean boxSafe(int row, int col, int num){
+	int startRow = row/3*3;
+	int startCol = col/3*3;
+	for (int i=0; i<3; i++){
+	    for (int ii=0; ii<3; ii++){
+		if (grid[startRow+i][startCol+ii]==num){
+		    return false;
+		}
+	    }
+	}
+	return true;
+    }
+    public boolean isSafe(int row, int col, int num){
+	return rowSafe(row,num)&&colSafe(col,num)&&boxSafe(row, col, num);
+    }
+    
 }
