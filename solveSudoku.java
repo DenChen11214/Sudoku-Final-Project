cd21fa9a1e0b1f15b9305a94c6b42641bf599483
Anthony <alam6@stuy.edu>
2018-01-19 21:38:48 -0500

    added countNums and addRandom methods

--- solveSudoku.java
+++ solveSudoku.java
@@ -1,10 +1,32 @@
 import java.util.*;
 public class solveSudoku{
-    private int[][] grid;
-
+    private int[][] puzzle;
+    private int[][] solution;
+    private Random randgen = new Random();
     public solveSudoku(){
     	grid = new int[9][9];
     	grid[1][2]=3;
+   	}
+   	public void addRandom(){
+   		while (countNums<10){
+   			int r = randgen.nextInt(9);
+   			int c = randgen.nextInt(9);
+   			int num = randgen.nextInt(9);
+   			if (isSafe(r,c,num)){
+   				grid[r][c]=num;
+   			}
+   		}
+   	}
+   	public int countNums(){
+   		int ans=0;
+   		for(int r=0;r<9;r++){
+	    	for (int c=0; c<9; c++){
+	    		if (grid[r][c]!=0){
+	    			ans++;
+	    		}
+	    	}
+	    }
+	    return ans;
    	}
     //checks whether or not a certain number (num) is already in the row
     public boolean rowsafe(int row, int num){
